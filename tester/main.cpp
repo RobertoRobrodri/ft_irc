0dcc964a8453be13a0c10ed21ee4f30887e40f19
mzomeno- <misaclumsy@gmail.com>
2023-07-20 19:07:29 +0200

    mark pending tests in main

--- tester/main.cpp
+++ tester/main.cpp
@@ -14,11 +14,34 @@ int main()
 	//test_connection(serv);
 
 	serv->init_pollfd();
+	
+	// fd_ready
+	
 	test_add_user(serv, 4, "63.161.169.137", 3490);
 	test_add_user(serv, 5, "63.161.169.138", 3490);
 	test_delete_user(serv, 2);
 	test_connection(serv);
 	test_parse_message(serv, "USER paco");
 	
+	// execute_command 
+
+	// user
+
+	// nick
+
+	// join
+
+	// pong
+
+	// quit
+
+	// privmsg
+
+	// topic
+
+	// invite
+
+	// kick	
+
 	return (0);
 }
