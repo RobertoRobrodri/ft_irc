feb95b023a3f11b0d80fb7966d8016345bc5b8ce
mzomeno- <misaclumsy@gmail.com>
2023-07-27 12:00:08 +0200

    give up fd_ready mock and delete test_connection typo

--- tester/main.cpp
+++ tester/main.cpp
@@ -15,14 +15,10 @@ int main()
 
 	serv->init_pollfd();
 	
-	// fd_ready
-	
 	test_add_user(serv, 4, "63.161.169.137", 3490);
 	test_add_user(serv, 5, "63.161.169.138", 3490);
 	test_delete_user(serv, 2);
-	test_connection(serv);
 	test_parse_message(serv, "USER paco");
-	
 	// execute_command 
 
 	// user
