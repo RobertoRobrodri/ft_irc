935951ea779900a4c2aee8b287e0bf87e0a89c0b
mzomeno- <misaclumsy@gmail.com>
2023-08-29 18:05:14 +0200

    fix realname missing parameter error

--- tester/command/user.cpp
+++ tester/command/user.cpp
@@ -21,16 +21,17 @@ void  cmd::username(server &svr, int poll_fd_pos, std::string str) {
   // Separar el resto del realname
   std::vector<std::string> cmd_params = ft_split(str, ':');
   std::vector<std::string> other_params = ft_split(cmd_params[0], ' ');
-  if (other_params.size() + 1 < 4 || cmd_params.empty())
+  if (other_params.size() + 1 < 4 || cmd_params.size() < 2)
   {
     svr.send_message(": 461 USER : <command> :Not enough parameters \r\n", usr.get_fd());
     return ;
   }
+  std::string realname = cmd_params[1];
   usr.set_username(other_params[0]);
   usr.set_hostname(other_params[1]);
   usr.set_servername(other_params[2]);
-  usr.set_realname(cmd_params[1]); //TODO sus
-
+  usr.set_realname(realname);
+ 
   // IF registered --> Send RPL_WELCOME
   //TODO meterlo en el define
   usr.is_registered(svr);
@@ -41,7 +42,7 @@ void	test_user_cmd(server *server)
 {
 	std::cout << BLUE << "Test user command\n";
 	std::cout << "==========================\n" << RESET;
-
+	
 	std::cout << CYAN << "Test 1: Valid command user 1\n" << RESET;
 	std::cout << YELLOW << "USER guest tolmoon tolsun :Ronnie Reagan\n" << RESET;
 	cmd::username(*server, 1, "guest tolmoon tolsun :Ronnie Reagan");
