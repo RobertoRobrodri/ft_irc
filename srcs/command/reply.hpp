ea3f099115743926ae58d358746ff2ac5b92cee4
mzomeno- <misaclumsy@gmail.com>
2023-08-30 12:48:23 +0200

    add new replies

--- srcs/command/reply.hpp
+++ srcs/command/reply.hpp
@@ -1,6 +1,9 @@
 // Todos los defines con los mensajes con los que tiene que responder el servidor
 #ifndef REPLY_HPP
 #define REPLY_HPP
-#define RPL_WELCOME(nick) ": 001 " + nick " : welcome " + nick + "\r\n"
+
+# define RPL_WELCOME(nick, user, host) ": 001 :Welcome to the Internet Relay Network " + nick + "!\n" + user + "@" + host + "\r\n"
+# define ERR_ALREADYREGISTERED ": 462 :You may not reregister\r\n"
+# define ERR_NEEDMOREPARAMS(command) ": 461 " + command + " :Not enough parameters\r\n"
 
 #endif
