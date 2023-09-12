a81bfdb0090f9e29a3cb53e9aed0a9a9cc3c4a6a
mzomeno- <misaclumsy@gmail.com>
2023-09-12 15:15:35 +0200

    remove comments

--- tester/command/quit.cpp
+++ tester/command/quit.cpp
@@ -5,9 +5,6 @@
 void  cmd::quit(server &svr, int poll_fd_pos, std::string str) {
   poll_fd pollfd = svr.get_pollfd(poll_fd_pos);
   user &usr = svr.get_user(pollfd.fd);
-  // BUG si no pasamos parámetros str == QUIT
-  // Qué pasa si quiero que mi mensaje sea QUIT?
-  // Este comportamiento se repite para cada comando
   if (str.empty())
 	str += "Cierra al salir (Mensaje por defecto del server)";
   str += "\r\n";
