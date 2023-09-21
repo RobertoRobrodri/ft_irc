2eec0abd69d53ad2def0945f858f54e3564490e2
mzomeno- <misaclumsy@gmail.com>
2023-09-21 14:43:53 +0200

    add replies

--- tester/command/part.cpp
+++ tester/command/part.cpp
@@ -2,13 +2,14 @@
 #include "reply.hpp"
 
 void  cmd::part(server &svr, int poll_fd_pos, std::string str) {
-  poll_fd pollfd = svr.get_pollfd(poll_fd_pos);
+	std::string command = "PART";
+	poll_fd pollfd = svr.get_pollfd(poll_fd_pos);
   user &usr = svr.get_user(pollfd.fd);
   if (usr.get_is_registered() == true)
   {
-    if (str == "PART")
+    if (str == "")
     {
-      svr.send_message(": 461: PART: Not enough parameters \r\n", usr.get_fd());
+      svr.send_message(ERR_NEEDMOREPARAMS(command), usr.get_fd());
       return ;
     }
     std::vector<std::string> chnlist = ft_split(str, ',');
@@ -17,12 +18,12 @@ void  cmd::part(server &svr, int poll_fd_pos, std::string str) {
       channel *chn = svr.get_channel_from_name(chnlist[i]);
       if (!chn)
       {
-          svr.send_message(": 403: " + chnlist[i] + " No such channel \r\n", usr.get_fd());
+          svr.send_message(ERR_NOSUCHCHANNEL(chnlist[i]), usr.get_fd());
           continue;
       }
       if (!chn->is_user_in_channel(usr))
       {
-        svr.send_message(": 442: " + chnlist[i] + " You're not on that channel \r\n", usr.get_fd());
+        svr.send_message(ERR_NOTONCHANNEL(chnlist[i]), usr.get_fd());
         continue ;
       }
       chn->rmv_member(usr);
