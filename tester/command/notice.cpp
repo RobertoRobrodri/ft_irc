54279b1abd9fc4625838c7ec8edac306042f6c3b
mzomeno- <misaclumsy@gmail.com>
2023-09-21 14:42:20 +0200

    fix notice

--- tester/command/notice.cpp
+++ tester/command/notice.cpp
@@ -2,14 +2,18 @@
 #include "reply.hpp"
 
 void  cmd::notice(server &svr, int poll_fd_pos, std::string str) {
-	std::string command = "NOTICE";
 	poll_fd pollfd = svr.get_pollfd(poll_fd_pos);
   user &usr = svr.get_user(pollfd.fd);
   if (usr.get_is_registered() == true)
   {
     if (str == "")
       return ;
-    std::vector<std::string> msglist = ft_split(str, ' ');
+	
+	std::vector<std::string> msglist = ft_split(str, ' ');
+	if (msglist.size() == 1)
+      return ;
+    
+	std::vector<std::string> rcvlist = ft_split(msglist[0], ',');
     std::string msg = str.substr(str.find(msglist[1]));
     for (size_t i = 0; i < rcvlist.size(); i++)
     {
@@ -17,12 +21,6 @@ void  cmd::notice(server &svr, int poll_fd_pos, std::string str) {
       {
         channel *chn = svr.get_channel_from_name(rcvlist[i]);
         usr.send_to_channel(svr, chn, rcvlist[i], msg);
-      }
-      else
-      {
-        user *receiver = svr.get_user_from_nick(rcvlist[i]);
-        if(receiver)
-          svr.send_message("From " + usr.get_nick() + ":\n" + msg + "\n", receiver->get_fd());
       }
     }
   }
