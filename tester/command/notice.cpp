23f8a729ea54b7a07baa194e0cceab711358df72
mzomeno- <misaclumsy@gmail.com>
2023-09-21 14:37:11 +0200

    notice command must not send error messages

--- tester/command/notice.cpp
+++ tester/command/notice.cpp
@@ -7,19 +7,9 @@ void  cmd::notice(server &svr, int poll_fd_pos, std::string str) {
   user &usr = svr.get_user(pollfd.fd);
   if (usr.get_is_registered() == true)
   {
-
     if (str == "")
-    {
-      svr.send_message(ERR_NORECIPIENT(command), usr.get_fd());
       return ;
-    }
     std::vector<std::string> msglist = ft_split(str, ' ');
-    if (msglist.size() == 1)
-    {
-      svr.send_message(": 412: No text to send \r\n", usr.get_fd());
-      return ;
-    }
-    std::vector<std::string> rcvlist = ft_split(msglist[0], ',');
     std::string msg = str.substr(str.find(msglist[1]));
     for (size_t i = 0; i < rcvlist.size(); i++)
     {
@@ -33,8 +23,6 @@ void  cmd::notice(server &svr, int poll_fd_pos, std::string str) {
         user *receiver = svr.get_user_from_nick(rcvlist[i]);
         if(receiver)
           svr.send_message("From " + usr.get_nick() + ":\n" + msg + "\n", receiver->get_fd());
-        else
-          svr.send_message(": 401 " + rcvlist[i] + ": No such nick/channel \r\n", usr.get_fd());
       }
     }
   }
