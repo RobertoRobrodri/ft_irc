a3d59ba2072a594dee0d327cc4fb584d43310b24
mzomeno- <misaclumsy@gmail.com>
2023-10-19 20:31:51 +0200

    QUIT removes user from channel list

--- srcs/command/quit.cpp
+++ srcs/command/quit.cpp
@@ -6,7 +6,7 @@
 /*   By: crisfern <crisfern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 11:19:08 by crisfern          #+#    #+#             */
-/*   Updated: 2023/10/02 13:42:39 by crisfern         ###   ########.fr       */
+/*   Updated: 2023/10/19 20:29:08 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,20 @@ void  cmd::quit(server &svr, int poll_fd_pos, std::string str) {
   user &usr = svr.get_user(pollfd.fd);
   if (str.empty())
 	  str = "Closing connection\r\n";
-  svr.send_message(str, usr.get_fd());
+
+  std::map<std::string, channel*>::iterator	chan_it;
+  channel	*chn;
+  for (chan_it = svr.list_of_channels.begin(); chan_it != svr.list_of_channels.end(); chan_it++)
+  {
+	  chn = chan_it->second;
+	  if (chn->is_user_in_channel(usr))
+	  {
+      	for (size_t i = 0; i < chn->get_list_of_members().size(); i++)
+        	if (chn->get_list_of_members()[i]->get_nick() != usr.get_nick())
+      			svr.send_message("From " + usr.get_nick() + ":\n" + str + "\n", \
+						chn->get_list_of_members()[i]->get_fd());
+	  	chn->rmv_member(usr);
+	  }
+  }
   svr.delete_user(poll_fd_pos);
 }
